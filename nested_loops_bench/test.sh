0a2ecb1525332d2d88ec3b95addd41e59c5ce271
Deepak Narayanan <deepakn94@gmail.com>
2016-09-22 22:41:32 -0700

    Sorry, just me being pedantic :(

--- nested_loops_bench/test.sh
+++ nested_loops_bench/test.sh
@@ -7,7 +7,7 @@ fi
 
 for it in 1 2 3 4 5; do
     for k in 10 100 1000; do
-        echo ----- $k ------
+        echo ----- $k -----
         for n in 10000 20000 30000 40000 50000 100000; do
             $CBO/nested_loops_bench/bench -k $k -n $n
         done
