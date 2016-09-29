5d67953c75d886c7a2351ce751ca5591c26712c1
Shoumik Palkar <shoumikpalkar@gmail.com>
2016-09-29 10:53:56 -0700

    Add random lookup benchmark and scripts

---
+++ randlookup_bench/test.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+if [ -z "$CBO" ]; then
+    echo "Set CBO to root of this repo."
+    exit 1
+fi  
+
+for _ in {1..5}; do
+    for k in 100 10000 100000 1000000 10000000 100000000 1000000000; do
+        echo ----- $k -----
+        $CBO/randlookup_bench/bench -k $k
+    done
+done
