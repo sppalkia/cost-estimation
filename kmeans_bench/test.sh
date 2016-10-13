1606ca3c45849abc57d23df25f388b9b95babd1b
Shoumik Palkar <shoumikpalkar@gmail.com>
2016-10-13 10:11:50 -0700

    More iterations

--- kmeans_bench/test.sh
+++ kmeans_bench/test.sh
@@ -12,7 +12,7 @@ for n in 256 512 1024 2048 4096 8192 16384; do
     for k in 2 4 256 512 1024 2048; do
         echo ----- $k -----
         for m in 2 4 8 128 256 512 1024 2048; do
-            $CBO/kmeans_bench/bench -n $n -k $k -m $m
+            $CBO/kmeans_bench/bench -n $n -k $k -m $m -i 5
         done
     done
 done
