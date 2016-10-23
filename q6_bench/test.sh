786b2f2d6b4d785024bca5d22b9676d2d59c643a
Shoumik Palkar <shoumik@csail.mit.edu>
2016-10-23 17:20:06 +0000

    Easy parse formatting

--- q6_bench/test.sh
+++ q6_bench/test.sh
@@ -6,7 +6,7 @@ if [ -z "$CBO" ]; then
 fi  
 
 for it in 1 2 3 4 5; do
-    for v in 1 5; do
+    for v in 1 5 10; do
         echo ----- $v -----
         for s in 0.01 0.1 0.25 0.5 0.75 0.9 1.0; do
             $CBO/q6_bench/bench -v $v -s $s
